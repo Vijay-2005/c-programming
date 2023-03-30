84ab0f7527adb5000d585e96081d8ff96ce7b12e
Vijay-2005 <111429863+Vijay-2005@users.noreply.github.com>
2023-03-30 10:11:08 +0530

    Add files via upload

---
+++ c programming/variables input output/basic.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    printf("hello world");
+    printf("\ngreeen world");
+    //variables
+    int x ;
+    x = 5;
+    printf( "\n%d" ,x);
+    int y , z ;
+    y = 4 ;
+    z = 7 ;
+    printf("\n%d",y+z);
+    printf("\n%d",y-z);
+    printf("\n%d",y*z);
+    printf("\n%d",y/z);
+    return 0 ;
+}
---
+++ c programming/variables input output/volume0fsphrere.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main (){
+    int r , area;
+    r = 4 ;
+    area = (4*3.14*r*r*r)/3;
+    printf("%d", area);
+    return 0 ;
+}
